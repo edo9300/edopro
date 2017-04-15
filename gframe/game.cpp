090dcc5f7c565d0b5b3fd3b2bbab5e9c3f606799
mercury233 <me@mercury233.me>
2017-04-16 00:47:29 +0800

    version

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -14,7 +14,7 @@
 #include <dirent.h>
 #endif
 
-const unsigned short PRO_VERSION = 0x2332;
+const unsigned short PRO_VERSION = 0x2334;
 
 namespace ygo {
 
@@ -103,12 +103,12 @@ bool Game::Initialize() {
 	env->addStaticText(L"已知问题：\n\n\
 场上没有可用于从额外卡组特殊召唤的\n\
 格子时仍然可以尝试进行从额外卡组的\n\
-同调·超量·融合召唤。此时同调·融\n\
-合素材会被送去墓地，超量素材会进入\n\
-异空间，但怪兽不会出场。\n\n\
-灵摆区域未被删除，但灵摆卡已经可以\n\
-正常发动到后场。\n\n\
-救援雪貂和王车连接的处理存在问题。", rect<s32>(300, 30, 550, 390), false, true, wMainMenu);
+超量·融合召唤。此时融合素材会被送\n\
+去墓地，超量素材会进入异空间，但怪\n\
+兽不会出场。\n\n\
+新大师规则下灵摆区域未被删除，但灵\n\
+摆卡已经可以正常发动到后场。\n\n\
+王车连接的处理存在问题。", rect<s32>(300, 30, 550, 390), false, true, wMainMenu);
 
 	//lan mode
 	wLanWindow = env->addWindow(rect<s32>(220, 100, 800, 520), false, dataManager.GetSysString(1200));
