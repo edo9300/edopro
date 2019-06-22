45a71578466fe16994c9313bd543ce71e21d2ea6
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-06-22 12:04:19 -0400

    Fix deploy script for macOS to correctly link OpenSSL

--- build-support/deploy-macOS.sh
+++ build-support/deploy-macOS.sh
@@ -6,6 +6,11 @@ mkdir -p deploy/ygopro.app/Contents/MacOS
 # Binary seems to be incorrectly named with the current premake
 cp bin/${BUILD_CONFIG:-release}/ygopro.app deploy/ygopro.app/Contents/MacOS/ygopro
 dylibbundler -x deploy/ygopro.app/Contents/MacOS/ygopro -b -d deploy/ygopro.app/Contents/Frameworks/ -p @executable_path/../Frameworks/ -cd
+# OpenSSL isn't in /usr/local/lib because Apple has deprecated it. 
+# libssl for some reason doesn't link to the libcrypto symlink in /usr/local/opt/openssl/lib,
+# but directly to the Cellar location, and this isn't caught by dylibbundler
+# This line likely needs to be updated if libcrypto's version ever changes, but not openssl's version
+install_name_tool -change /usr/local/Cellar/openssl/$(brew list --versions openssl | cut -d " " -f 2)/lib/libcrypto.1.0.0.dylib @executable_path/../Frameworks/libcrypto.1.0.0.dylib deploy/ygopro.app/Contents/Frameworks/libssl.1.0.0.dylib
 cp strings.conf deploy/strings.conf
 cp system.conf deploy/system.conf
 cp -r textures deploy/textures
