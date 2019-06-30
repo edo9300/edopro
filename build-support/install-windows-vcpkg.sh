dbe17717e2f51e8d66c5717e30270a7742d1948a
kevinlul <6320810+kevinlul@users.noreply.github.com>
2019-06-30 18:21:50 -0400

    Add vcpkg install script

---
+++ build-support/install-windows-vcpkg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ ! $VCPKG_ROOT ]]; then
+    export VCPKG_ROOT=/c/vcpkg
+    git clone https://github.com/Microsoft/vcpkg.git $VCPKG_ROOT
+    cd $VCPKG_ROOT
+    powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '.\bootstrap-vcpkg.bat'"
+    ./vcpkg.exe integrate install
+    ./vcpkg.exe integrate powershell # optional
+    cd -
+fi
+export VCPKG_DEFAULT_TRIPLET=${1:-x86-windows-static}
+$VCPKG_ROOT/vcpkg.exe install freetype libevent lua[cpp] sqlite3 fmt curl libgit2 nlohmann-json
