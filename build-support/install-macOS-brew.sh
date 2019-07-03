c09389f8d1f64cbf46d09e1b67a2d3e8b5b67b6a
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-07-03 15:40:14 -0400

    Build patched irrlicht from source instead on macOS

--- build-support/install-macOS-brew.sh
+++ build-support/install-macOS-brew.sh
@@ -3,4 +3,4 @@
 if [[ $(command -v brew) == "" ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-brew install dylibbundler p7zip libevent fmt freetype irrlicht sqlite curl libgit2 nlohmann-json
+brew install dylibbundler gpatch p7zip libevent fmt freetype sqlite curl libgit2 nlohmann-json
--- build-support/install-macOS-src.sh
+++ build-support/install-macOS-src.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+cp irrlicht/irrlicht-macOS.patch /tmp
+cd /tmp
+curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://downloads.sourceforge.net/irrlicht/irrlicht-1.8.4.zip
+7z x irrlicht-1.8.4.zip
+cd irrlicht-1.8.4
+/usr/local/opt/gpatch/bin/patch -p0 --binary -i ../irrlicht-macOS.patch
+xcodebuild -project source/Irrlicht/MacOSX/MacOSX.xcodeproj -configuration Release -target libIrrlicht.a SYMROOT=build
+sudo mkdir -p /usr/local/include/irrlicht
+sudo cp -r include/*.h /usr/local/include/irrlicht
+sudo cp source/Irrlicht/MacOSX/build/Release/libIrrlicht.a /usr/local/lib
+
 # Installs lua for C++ to /usr/local from source because brew's version is wonky
 cd /tmp
 curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://www.lua.org/ftp/lua-5.3.5.tar.gz
