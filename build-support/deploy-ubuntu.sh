574d0264978770f8d752a54398cac2c744031ec2
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-07-11 12:10:15 -0400

    Update font to Noto Sans JP
    Fix edo9300/ygopro#17

--- build-support/deploy-ubuntu.sh
+++ build-support/deploy-ubuntu.sh
@@ -14,4 +14,4 @@ cp *.conf deploy/
 cp *.json deploy/
 if [[ ! -d deploy/textures ]]; then cp -r textures deploy/; fi
 curl --retry 5 --connect-timeout 30 --location --remote-header-name --output deploy/cards.cdb https://github.com/YgoproStaff/live2019/raw/master/cards.cdb
-curl --retry 5 --connect-timeout 30 --location --remote-header-name --output deploy/fonts/OpenSans-Regular.ttf https://github.com/google/fonts/raw/master/apache/opensans/OpenSans-Regular.ttf
+curl --retry 5 --connect-timeout 30 --location --remote-header-name --output deploy/fonts/NotoSansJP-Regular.otf https://github.com/googlefonts/noto-cjk/raw/master/NotoSansJP-Regular.otf
