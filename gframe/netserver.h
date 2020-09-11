f6ae18f3c135e98114dca2cec7689f7450a4bb6a
edo9300 <edoardo762@gmail.com>
2020-09-11 17:22:36 +0200

    Use unsigned sizes in SendBufferToPlayer and SendPacketToPlayer

--- gframe/netserver.h
+++ gframe/netserver.h
@@ -33,9 +33,9 @@ public:
 	static void HandleCTOSPacket(DuelPlayer* dp, char* data, unsigned int len);
 	static void SendPacketToPlayer(DuelPlayer* dp, unsigned char proto) {
 		char* p = net_server_write;
-		BufferIO::WriteInt16(p, 1);
-		BufferIO::WriteInt8(p, proto);
-		last_sent = 3;
+		BufferIO::Write<uint16_t>(p, 1u);
+		BufferIO::Write<uint8_t>(p, proto);
+		last_sent = 3u;
 		if(!dp)
 			return;
 		bufferevent_write(dp->bev, net_server_write, last_sent);
@@ -43,19 +43,19 @@ public:
 	template<typename ST>
 	static void SendPacketToPlayer(DuelPlayer* dp, unsigned char proto, ST& st) {
 		char* p = net_server_write;
-		BufferIO::WriteInt16(p, 1 + sizeof(ST));
-		BufferIO::WriteInt8(p, proto);
+		BufferIO::Write<uint16_t>(p, 1u + sizeof(ST));
+		BufferIO::Write<uint8_t>(p, proto);
 		memcpy(p, &st, sizeof(ST));
-		last_sent = sizeof(ST) + 3;
+		last_sent = sizeof(ST) + 3u;
 		if(dp)
 			bufferevent_write(dp->bev, net_server_write, last_sent);
 	}
 	static void SendBufferToPlayer(DuelPlayer* dp, unsigned char proto, void* buffer, size_t len) {
 		char* p = net_server_write;
-		BufferIO::WriteInt16(p, 1 + len);
-		BufferIO::WriteInt8(p, proto);
+		BufferIO::Write<uint16_t>(p, 1u + len);
+		BufferIO::Write<uint8_t>(p, proto);
 		memcpy(p, buffer, len);
-		last_sent = len + 3;
+		last_sent = len + 3u;
 		if(dp)
 			bufferevent_write(dp->bev, net_server_write, last_sent);
 	}
