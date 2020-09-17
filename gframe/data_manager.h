e647c0b2f8e627a25dc5a85cf8a7d7e0097f6e1c
KNIGHT <perfectdicky@yahoo.com.hk>
2020-09-17 21:45:50 +0800

    fix level for -ve

--- gframe/data_manager.h
+++ gframe/data_manager.h
@@ -33,7 +33,10 @@ struct CardData {
 	uint32_t alias;
 	uint16_t* setcodes;
 	uint32_t type;
-	uint32_t level;
+	//////kdiy///////
+	//uint32_t level;
+	int32_t level;
+	//////kdiy///////	
 	uint32_t attribute;
 	uint32_t race;
 	int32_t attack;
@@ -47,7 +50,10 @@ struct CardDataC {
 	uint32_t alias;
 	uint16_t* setcodes_p;
 	uint32_t type;
-	uint32_t level;
+	//////kdiy///////
+	//uint32_t level;
+	int32_t level;
+	//////kdiy///////	
 	uint32_t attribute;
 	uint32_t race;
 	int32_t attack;
