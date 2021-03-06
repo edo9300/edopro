3eab0fc11718ede369dc4ed94e37ec89a2fbb3b6
KNIGHT <perfectdicky@yahoo.com.hk>
2021-03-07 02:44:27 +0800

    merge same code's setnames

--- gframe/deck_con.cpp
+++ gframe/deck_con.cpp
@@ -51,13 +51,16 @@ static int parse_filter(const wchar_t* pstr, uint32_t& type) {
 
 static bool check_set_code(const CardDataC& data, const std::vector<uint32_t>& setcodes) {
 	auto card_setcodes = data.setcodes;
-	///kdiy/////////
-	//if (data.alias) {
-	if (data.alias && setcodes.empty()) {	
-	///kdiy/////////		
+	if (data.alias) {		
 		auto _data = gDataManager->GetCardData(data.alias);
 		if(_data)
-			card_setcodes = _data->setcodes;
+		    ///kdiy/////////
+			//card_setcodes = _data->setcodes;
+			{
+				for(auto& set_code : _data->setcodes)
+					card_setcodes.push_back(set_code);
+			}
+			///kdiy/////////
 	}
 	if(setcodes.empty())
 		return card_setcodes.empty();
