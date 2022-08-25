4f2e53303d13c74992d6b31b4e232eb578c8cc7d
Edoardo Lolletti <edoardo762@gmail.com>
2022-08-25 17:46:59 +0200

    Update vcpkg cache url
    Osx irrlicht manual moving no longer needed

--- travis/get-osx-vcpkg-cache.sh
+++ travis/get-osx-vcpkg-cache.sh
@@ -7,6 +7,3 @@ mkdir -p "$VCPKG_ROOT"
 cd "$VCPKG_ROOT"
 curl --retry 5 --connect-timeout 30 --location --remote-header-name --output installed.7z $VCPKG_CACHE_7Z_URL
 7z x installed.7z
-mkdir -p ./installed/$ARCH-osx/include/irrlicht/
-mv irrlicht/include/* ./installed/$ARCH-osx/include/irrlicht
-mv irrlicht/lib/* ./installed/$ARCH-osx/lib
