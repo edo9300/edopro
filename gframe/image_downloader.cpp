325fa0db0fa18f9bf39540ea661b5eda247e1352
Edoardo Lolletti <edoardo762@gmail.com>
2022-09-17 22:47:30 +0200

    Set CURLOPT_CONNECTTIMEOUT in image downloader
    Prevents softlocking on exit if curl coudln't connect to the image url

--- gframe/image_downloader.cpp
+++ gframe/image_downloader.cpp
@@ -88,6 +88,8 @@ void ImageDownloader::DownloadPic() {
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &payload);
 	curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
+	curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30L);
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
 	if(gGameConfig->ssl_certificate_path.size() && Utils::FileExists(Utils::ToPathString(gGameConfig->ssl_certificate_path)))
 		curl_easy_setopt(curl, CURLOPT_CAINFO, gGameConfig->ssl_certificate_path.data());
 #ifdef _WIN32
