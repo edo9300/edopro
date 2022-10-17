f1727d2504c10a98c2c57a55c8b96c345992b200
Edoardo Lolletti <edoardo762@gmail.com>
2022-10-17 17:29:59 +0200

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 cd $DEPLOY_DIR
 git init
 git checkout --orphan $DEPLOY_BRANCH
-git config user.email deploy@travis-ci.org
-git config user.name "Deployment Bot (from Travis CI)"
+git config user.email github-actions[bot]@users.noreply.github.com
+git config user.name "github-actions[bot]"
 git add -A .
 git commit -qm "Deploy $DEPLOY_REPO to $DEPLOY_REPO:$DEPLOY_BRANCH"
 git push -qf https://$DEPLOY_TOKEN@github.com/$DEPLOY_REPO.git $DEPLOY_BRANCH:$DEPLOY_BRANCH
