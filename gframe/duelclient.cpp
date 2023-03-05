edo9300/edopro

commit b6af0562f8d9d899964809c51369936c87bb87de

Fix various integer casting to smaller size warnings

gframe/duelclient.cpp
@@ -114,8 +114,8 @@ bool DuelClient::StartClient(uint32_t ip, uint16_t port, uint32_t gameid, bool c
 	rnd.seed(time(0));
 	if(!create_game) {
 		timeval timeout = {5, 0};
-		event* resp_event = event_new(client_base, 0, EV_TIMEOUT, ConnectTimeout, 0);
-		event_add(resp_event, &timeout);
+		event* timeout_event = event_new(client_base, 0, EV_TIMEOUT, ConnectTimeout, 0);
+		event_add(timeout_event, &timeout);
 	}
 	mainGame->dInfo.secret.game_id = gameid;
 	mainGame->dInfo.secret.server_port = port;
@@ -132,6 +132,9 @@ bool DuelClient::StartClient(uint32_t ip, uint16_t port, uint32_t gameid, bool c
 	return true;
 }
 void DuelClient::ConnectTimeout(evutil_socket_t fd, short events, void* arg) {
+	(void)fd;
+	(void)events;
+	(void)arg;
 	if(connect_state & 0x7)
 		return;
 	if(!is_closing) {
@@ -175,6 +178,7 @@ void DuelClient::StopClient(bool is_exiting) {
 	mainGame->frameSignal.SetNoWait(false);
 }
 void DuelClient::ClientRead(bufferevent* bev, void* ctx) {
+	(void)ctx;
 	evbuffer* input = bufferevent_get_input(bev);
 	size_t len = evbuffer_get_length(input);
 	uint16_t packet_len = 0;
@@ -196,6 +200,7 @@ void DuelClient::ClientRead(bufferevent* bev, void* ctx) {
 #define INTERNAL_HANDLE_CONNECTION_END 0
 
 void DuelClient::ClientEvent(bufferevent *bev, short events, void *ctx) {
+	(void)bev;
 	if (events & BEV_EVENT_CONNECTED) {
 		bool create_game = (size_t)ctx != 0;
 		CTOS_PlayerInfo cspi;
@@ -699,7 +704,7 @@ void DuelClient::HandleSTOCPacketLanAsync(const std::vector<uint8_t>& data) {
 		mainGame->dInfo.duel_params = params;
 		mainGame->dInfo.isRelay = params & DUEL_RELAY;
 		params &= ~DUEL_RELAY;
-		pkt.info.no_shuffle_deck = pkt.info.no_shuffle_deck || ((params & DUEL_PSEUDO_SHUFFLE) != 0);
+		pkt.info.no_shuffle_deck = (pkt.info.no_shuffle_deck != 0) || ((params & DUEL_PSEUDO_SHUFFLE) != 0);
 		params &= ~DUEL_PSEUDO_SHUFFLE;
 		mainGame->dInfo.team1 = pkt.info.team1;
 		mainGame->dInfo.team2 = pkt.info.team2;
@@ -1407,8 +1412,8 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 			std::vector<std::wstring> tmp;
 			for(uint32_t filter = 0x1; filter != 0; filter <<= 1) {
 				if(uint32_t s = filter & data) {
-					uint32_t player_string;
-					uint32_t zone_string;
+					uint32_t player_string{};
+					uint32_t zone_string{};
 					uint32_t seq = 1;
 					if(s & 0x60) {
 						player_string = 1081;
@@ -2507,7 +2512,6 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 		uint8_t c;
 		std::vector<ClientCard*> field_confirm;
 		std::vector<ClientCard*> panel_confirm;
-		ClientCard* pcard;
 		if(mainGame->dInfo.isCatchingUp)
 			return true;
 		mainGame->AddLog(epro::sprintf(gDataManager->GetSysString(208), count));
@@ -2517,6 +2521,7 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 			l = BufferIO::Read<uint8_t>(pbuf);
 			s = CompatRead<uint8_t, uint32_t>(pbuf);
 			std::unique_lock<epro::mutex> lock(mainGame->gMutex);
+			ClientCard* pcard;
 			if (l == 0) {
 				pcard = new ClientCard{};
 				pcard->sequence = static_cast<uint32_t>(mainGame->dField.limbo_temp.size());
@@ -2545,7 +2550,7 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 						panel_confirm.push_back(pcard);
 				}
 			} else {
-				if(!mainGame->dInfo.isReplay || (l & LOCATION_ONFIELD) | (l & LOCATION_HAND && gGameConfig->hideHandsInReplays))
+				if(!mainGame->dInfo.isReplay || (l & LOCATION_ONFIELD) || (l & LOCATION_HAND && gGameConfig->hideHandsInReplays))
 					field_confirm.push_back(pcard);
 			}
 		}
@@ -3445,9 +3450,9 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 		const auto player = mainGame->LocalPlayer(BufferIO::Read<uint8_t>(pbuf));
 		const auto count = CompatRead<uint8_t, uint32_t>(pbuf);
 		auto lock = LockIf();
-		ClientCard* pcard;
-		for(uint32_t i = 0; i < count; ++i) {
-			pcard = mainGame->dField.GetCard(player, LOCATION_DECK, mainGame->dField.deck[player].size() - 1 - i);
+		auto& deck = mainGame->dField.deck[player];
+		for (auto it = deck.rbegin(), end = it + 5; it != end; ++it) {
+			auto pcard = *it;
 			const auto code = BufferIO::Read<uint32_t>(pbuf);
 			if(!mainGame->dInfo.compat_mode) {
 				/*uint32_t position =*/BufferIO::Read<uint32_t>(pbuf);
@@ -3459,12 +3464,12 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 		}
 		for(uint32_t i = 0; i < count; ++i) {
 			Play(SoundManager::SFX::DRAW);
-			pcard = mainGame->dField.GetCard(player, LOCATION_DECK, mainGame->dField.deck[player].size() - 1);
-			mainGame->dField.deck[player].pop_back();
+			const auto pcard = deck.back();
+			deck.pop_back();
 			mainGame->dField.AddCard(pcard, player, LOCATION_HAND, 0);
 			if(!mainGame->dInfo.isCatchingUp) {
-				for(auto& pcard : mainGame->dField.hand[player])
-					mainGame->dField.MoveCard(pcard, 10);
+				for(auto& hand_pcard : mainGame->dField.hand[player])
+					mainGame->dField.MoveCard(hand_pcard, 10);
 				mainGame->WaitFrameSignal(5, lock);
 			}
 		}
@@ -3709,13 +3714,13 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 			return true;
 		CoreUtils::loc_info info1 = CoreUtils::ReadLocInfo(pbuf, mainGame->dInfo.compat_mode);
 		info1.controler = mainGame->LocalPlayer(info1.controler);
-		const auto aatk = BufferIO::Read<uint32_t>(pbuf);
-		const auto adef = BufferIO::Read<uint32_t>(pbuf);
+		const auto aatk = static_cast<int32_t>(BufferIO::Read<uint32_t>(pbuf));
+		const auto adef = static_cast<int32_t>(BufferIO::Read<uint32_t>(pbuf));
 		/*const auto da = */BufferIO::Read<uint8_t>(pbuf);
 		CoreUtils::loc_info info2 = CoreUtils::ReadLocInfo(pbuf, mainGame->dInfo.compat_mode);
 		info2.controler = mainGame->LocalPlayer(info2.controler);
-		const auto datk = BufferIO::Read<uint32_t>(pbuf);
-		const auto ddef = BufferIO::Read<uint32_t>(pbuf);
+		const auto datk = static_cast<int32_t>(BufferIO::Read<uint32_t>(pbuf));
+		const auto ddef = static_cast<int32_t>(BufferIO::Read<uint32_t>(pbuf));
 		/*const auto dd = */BufferIO::Read<uint8_t>(pbuf);
 		std::lock_guard<epro::mutex> lock(mainGame->gMutex);
 		ClientCard* pcard = mainGame->dField.GetCard(info1.controler, info1.location, info1.sequence);

gframe/CGUICustomContextMenu/CGUICustomContextMenu.cpp
@@ -286,12 +286,12 @@ bool CGUICustomContextMenu::OnEvent(const SEvent& event) {
 							IGUIElement * p = EventParent ? EventParent : Parent;
 							setEventParent(p);
 
-							SEvent event;
-							event.EventType = EET_GUI_EVENT;
-							event.GUIEvent.Caller = this;
-							event.GUIEvent.Element = 0;
-							event.GUIEvent.EventType = EGET_ELEMENT_CLOSED;
-							if(!p->OnEvent(event)) {
+							SEvent closed_event;
+							closed_event.EventType = EET_GUI_EVENT;
+							closed_event.GUIEvent.Caller = this;
+							closed_event.GUIEvent.Element = 0;
+							closed_event.GUIEvent.EventType = EGET_ELEMENT_CLOSED;
+							if(!p->OnEvent(closed_event)) {
 								if(CloseHandling & ECMC_HIDE) {
 									setVisible(false);
 								}

gframe/CGUITTFont/CGUITTFont.cpp
@@ -285,36 +285,38 @@ static SGUITTFace* OpenFileStreamFont(FT_Library library, io::IReadFile* file) {
 	return new SGUITTFace{ freetype_face };
 }
 
+#ifdef YGOPRO_USE_BUNDLED_FONT
 static SGUITTFace* OpenMemoryStreamFont(FT_Library library, const void* data, size_t size) {
 	FT_Face freetype_face = nullptr;
 	if(FT_New_Memory_Face(library, static_cast<const FT_Byte*>(data), static_cast<FT_Long>(size), 0, &freetype_face) != FT_Err_Ok)
 		return nullptr;
 	return new SGUITTFace{ freetype_face };
 }
+#endif
 
-bool CGUITTFont::load(const io::path& filename, const u32 size, const bool antialias, const bool transparency) {
+bool CGUITTFont::load(const io::path& font_filename, const u32 font_size, const bool antialias, const bool transparency) {
 	// Some sanity checks.
 	if(Environment == nullptr || Driver == nullptr) return false;
-	if(size == 0) return false;
-	if(filename.empty()) return false;
+	if(font_size == 0) return false;
+	if(font_filename.empty()) return false;
 
 	io::IFileSystem* filesystem = Environment->getFileSystem();
 	irr::ILogger* logger = (Device != 0 ? Device->getLogger() : 0);
-	this->size = size;
-	this->filename = filename;
+	size = font_size;
+	filename = font_filename;
 
 	// Update the font loading flags when the font is first loaded.
-	this->use_monochrome = !antialias;
-	this->use_transparency = transparency;
+	use_monochrome = !antialias;
+	use_transparency = transparency;
 	update_load_flags();
 
 	// Log.
 	if(logger)
-		logger->log(L"CGUITTFont", core::stringw(core::stringw(L"Creating new font: ") + core::stringc(filename) + L" " + core::stringc(size) + L"pt " + (antialias ? L"+antialias " : L"-antialias ") + (transparency ? L"+transparency" : L"-transparency")).c_str(), irr::ELL_INFORMATION);
+		logger->log(L"CGUITTFont", core::stringw(core::stringw(L"Creating new font: ") + core::stringc(font_filename) + L" " + core::stringc(font_size) + L"pt " + (antialias ? L"+antialias " : L"-antialias ") + (transparency ? L"+transparency" : L"-transparency")).c_str(), irr::ELL_INFORMATION);
 
 	// Grab the face.
 	SGUITTFace* face = nullptr;
-	auto* node = c_faces.find(filename);
+	auto* node = c_faces.find(font_filename);
 	if(node == nullptr) {
 #ifdef YGOPRO_USE_BUNDLED_FONT
 		if(filename.equals_ignore_case(_IRR_TEXT("bundled")))
@@ -323,7 +325,7 @@ bool CGUITTFont::load(const io::path& filename, const u32 size, const bool antia
 #endif //YGOPRO_USE_BUNDLED_FONT
 		if(filesystem) {
 			// Read in the file data.
-			io::IReadFile* file = filesystem->createAndOpenFile(filename);
+			io::IReadFile* file = filesystem->createAndOpenFile(font_filename);
 			if(file == nullptr) {
 				if(logger) logger->log(L"CGUITTFont", L"Failed to open the file.", irr::ELL_INFORMATION);
 				return false;
@@ -335,7 +337,7 @@ bool CGUITTFont::load(const io::path& filename, const u32 size, const bool antia
 			if(logger) logger->log(L"CGUITTFont", L"OpenFileStreamFont failed.", irr::ELL_INFORMATION);
 			return false;
 		}
-		c_faces.set(filename, face);
+		c_faces.set(font_filename, face);
 	} else {
 		// Using another instance of this face.
 		face = node->getValue();
@@ -346,7 +348,7 @@ bool CGUITTFont::load(const io::path& filename, const u32 size, const bool antia
 	tt_face = face->face;
 
 	// Store font metrics.
-	FT_Set_Pixel_Sizes(tt_face, size, 0);
+	FT_Set_Pixel_Sizes(tt_face, font_size, 0);
 	font_metrics = tt_face->size->metrics;
 
 	// Allocate our glyphs.
@@ -713,18 +715,18 @@ u32 CGUITTFont::getGlyphIndexByChar(wchar_t c, core::array<SGUITTGlyph>** glyphs
 
 u32 CGUITTFont::getGlyphIndexByChar(uchar32_t c, core::array<SGUITTGlyph>** glyphs, core::array<CGUITTGlyphPage*>** glyphpages, bool called_as_fallback) const {
 	// Get the glyph.
-	u32 glyph = FT_Get_Char_Index(tt_face, c);
+	u32 glyph_index = FT_Get_Char_Index(tt_face, c);
 
-	if(glyph == 0 && fallback && glyphs) {
-		glyph = fallback->getGlyphIndexByChar(c, glyphs, glyphpages, true);
-		if(glyph)
-			return glyph;
+	if(glyph_index == 0 && fallback && glyphs) {
+		glyph_index = fallback->getGlyphIndexByChar(c, glyphs, glyphpages, true);
+		if(glyph_index)
+			return glyph_index;
 	}
 
 	// Check for a valid glyph.  If it is invalid, attempt to use the replacement character.
-	if(glyph == 0) {
+	if(glyph_index == 0) {
 		if(!called_as_fallback)
-			glyph = FT_Get_Char_Index(tt_face, core::unicode::UTF_REPLACEMENT_CHARACTER);
+			glyph_index = FT_Get_Char_Index(tt_face, core::unicode::UTF_REPLACEMENT_CHARACTER);
 		else
 			return 0;
 	}
@@ -736,8 +738,8 @@ u32 CGUITTFont::getGlyphIndexByChar(uchar32_t c, core::array<SGUITTGlyph>** glyp
 		*glyphpages = &Glyph_Pages;
 
 	// If our glyph is already loaded, don't bother doing any batch loading code.
-	if(glyph != 0 && Glyphs[glyph - 1].isLoaded)
-		return glyph;
+	if(glyph_index != 0 && Glyphs[glyph_index - 1].isLoaded)
+		return glyph_index;
 
 	// Determine our batch loading positions.
 	u32 half_size = (batch_load_size / 2);
@@ -761,7 +763,7 @@ u32 CGUITTFont::getGlyphIndexByChar(uchar32_t c, core::array<SGUITTGlyph>** glyp
 	} while(++start_pos < end_pos);
 
 	// Return our original character.
-	return glyph;
+	return glyph_index;
 }
 
 void CGUITTFont::clearGlyphPages() {
@@ -914,7 +916,7 @@ CGUITTGlyphPage::~CGUITTGlyphPage() {
 	}
 }
 
-bool CGUITTGlyphPage::createPageTexture(const u8 & pixel_mode, const core::dimension2du & texture_size) {
+bool CGUITTGlyphPage::createPageTexture(const u8 & pixel_mode, const core::dimension2du & target_texture_size) {
 	if(texture)
 		return false;
 
@@ -924,11 +926,11 @@ bool CGUITTGlyphPage::createPageTexture(const u8 & pixel_mode, const core::dimen
 	// Set the texture color format.
 	switch(pixel_mode) {
 		case FT_PIXEL_MODE_MONO:
-			texture = driver->addTexture(texture_size, name, video::ECF_A1R5G5B5);
+			texture = driver->addTexture(target_texture_size, name, video::ECF_A1R5G5B5);
 			break;
 		case FT_PIXEL_MODE_GRAY:
 		default:
-			texture = driver->addTexture(texture_size, name, video::ECF_A8R8G8B8);
+			texture = driver->addTexture(target_texture_size, name, video::ECF_A8R8G8B8);
 			break;
 	}
 

gframe/CGUITTFont/CGUITTFont.h
@@ -68,7 +68,7 @@ public:
 //! Structure representing a single TrueType glyph.
 struct SGUITTGlyph {
 	//! Constructor.
-	SGUITTGlyph() : isLoaded(false), glyph_page(0), surface(0), parent(0) {}
+	SGUITTGlyph() : isLoaded(false), glyph_page(0), advance({}), surface(0), parent(0) {}
 
 	//! Destructor.
 	~SGUITTGlyph() {

gframe/CGUITTFont/irrUString.h
@@ -442,6 +442,10 @@ public:
 	}
 
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4127) //conditional expression is constant
+#endif
 	//! Returns the length of a ustring16 in full characters.
 	//! \return Length of a ustring16 in full characters.
 	u32 size() const {
@@ -462,6 +466,9 @@ public:
 		}
 		return size_;
 	}
+#ifdef _MSC_VER
+#pragma warning(pop) //conditional expression is constant
+#endif
 
 
 	//! Informs if the ustring is empty or not.
@@ -515,6 +522,10 @@ public:
 	}
 
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4127) //conditional expression is constant
+#endif
 	//! Validate the existing ustring16, checking for valid surrogate pairs and checking for proper termination.
 	//! \return A reference to our current string.
 	void validate() {
@@ -524,6 +535,9 @@ public:
 			return;
 		}
 	}
+#ifdef _MSC_VER
+#pragma warning(pop) //conditional expression is constant
+#endif
 
 
 	//! Returns the raw number of UTF-16 code points in the string which includes the individual surrogates.

gframe/CProgressBar/CProgressBar.cpp
@@ -17,9 +17,9 @@ IProgressBar::IProgressBar(irr::gui::IGUIEnvironment* guienv, const irr::core::r
 	progress = 0;
 	resize_ratio = irr::core::vector2df(0, 0);
 }
-void IProgressBar::setColors(irr::video::SColor progress, irr::video::SColor filling) {
-	fillcolor = progress;
-	emptycolor = filling;
+void IProgressBar::setColors(irr::video::SColor new_fillcolor, irr::video::SColor new_emptycolor) {
+	fillcolor = new_fillcolor;
+	emptycolor = new_emptycolor;
 }
 void IProgressBar::addBorder(irr::s32 size, irr::video::SColor color) {
 	bordercolor = color;

gframe/CProgressBar/CProgressBar.h
@@ -18,7 +18,7 @@ public:
 	void setProgress(irr::s32 progress);
 
 	/*Set bar Colors*/
-	void setColors(irr::video::SColor progress = irr::video::SColor(255, 255, 255, 255), irr::video::SColor filling = irr::video::SColor(255, 0, 0, 0));
+	void setColors(irr::video::SColor fillcolor = irr::video::SColor(255, 255, 255, 255), irr::video::SColor emptycolor = irr::video::SColor(255, 0, 0, 0));
 
 	/*Allow you to add a "border" into your bar. You MUST specify the size (of course in pixel) of the border. You can also pass a color parameter (Black by default)*/
 	void addBorder(irr::s32 size, irr::video::SColor color = irr::video::SColor(255, 0, 0, 0));

gframe/CXMLRegistry/CXMLNode.cpp
@@ -67,12 +67,11 @@ void CXMLNode::drop() {
 		children.clear();
 	}
 }
-CXMLNode* CXMLNode::findChildByName(const wchar_t* name) {
-	core::list<CXMLNode*>::Iterator	it;
+CXMLNode* CXMLNode::findChildByName(const wchar_t* child_name) const {
 	core::stringw tmp;
 	CXMLNode* node = NULL;
-	tmp = name;
-	for(it = children.begin(); it != children.end(); it++) {
+	tmp = child_name;
+	for(auto it = children.begin(); it != children.end(); it++) {
 		if(tmp.equals_ignore_case((*it)->getName())) {
 			node = (*it);
 			break;

commit c7bcae29aab8e9dd7bf18d7e30ff618b7933449e

Fix bug introduced by a typo in b6af0562
When parsing MSG_DRAW 5 cards were always attempted to be accessed from the deck instead of the passed amount, thus leading to a corrupt state of the deck for the client and possibly a crash as soon as a player's deck went below 5 cards

gframe/duelclient.cpp
@@ -3454,7 +3454,7 @@ int DuelClient::ClientAnalyze(const uint8_t* msg, uint32_t len) {
 		const auto count = CompatRead<uint8_t, uint32_t>(pbuf);
 		auto lock = LockIf();
 		auto& deck = mainGame->dField.deck[player];
-		for (auto it = deck.rbegin(), end = it + 5; it != end; ++it) {
+		for (auto it = deck.crbegin(), end = it + count; it != end; ++it) {
 			auto pcard = *it;
 			const auto code = BufferIO::Read<uint32_t>(pbuf);
 			if(!mainGame->dInfo.compat_mode) {