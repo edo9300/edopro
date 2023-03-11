bbc66ba3d63f124de23ad439915a6a68cd38e2c1
Edoardo Lolletti <edoardo762@gmail.com>
2023-03-11 23:54:37 +0100

    Fix typo from 7d3628ff

--- gframe/client_card.cpp
+++ gframe/client_card.cpp
@@ -120,7 +120,7 @@ void ClientCard::ClearTarget() {
 	}
 	for(auto& pcard : ownerTarget) {
 		pcard->is_showtarget = false;
-		pcard->ownerTarget.erase(this);
+		pcard->cardTarget.erase(this);
 	}
 	cardTarget.clear();
 	ownerTarget.clear();
