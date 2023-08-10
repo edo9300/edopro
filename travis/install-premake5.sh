f11dba71333dd3b7ed33aaef46de896fa9b1fe43
Edoardo Lolletti <edoardo762@gmail.com>
2023-08-10 10:52:38 +0200

    Use premake beta2
    Use premake 5.0.0-beta1 for ubuntu 18 runners

--- travis/install-premake5.sh
+++ travis/install-premake5.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 TRAVIS_OS_NAME=${TRAVIS_OS_NAME:-$1}
-PREMAKE_VERSION=${PREMAKE_VERSION:-5.0.0-beta1}
+PREMAKE_VERSION=${PREMAKE_VERSION:-5.0.0-beta2}
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
 	curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://github.com/premake/premake-core/releases/download/v$PREMAKE_VERSION/premake-$PREMAKE_VERSION-windows.zip
