669c6836efcf88163092ca21b028f001e3e47a57
Edoardo Lolletti <edoardo762@gmail.com>
2024-09-06 10:28:46 +0200

    Cleanup fmt header
    Remove custom formatters for nonstd string_view and simplify code

--- gframe/address.h
+++ gframe/address.h
@@ -60,7 +60,7 @@ struct fmt::formatter<epro::Address, T> {
 	template<typename FormatContext>
 	auto format(const epro::Address& address, FormatContext& ctx) const {
 		static constexpr auto format_str = CHAR_T_STRINGVIEW(T, "{}");
-		return fmt::format_to(ctx.out(), format_str.data(), address.format<T>());
+		return format_to(ctx.out(), format_str, address.format<T>());
 	}
 };
 
--- gframe/fmt.h
+++ gframe/fmt.h
@@ -6,23 +6,6 @@
 #include <fmt/core.h>
 static_assert(FMT_VERSION >= 50000, "Fmt 5.0.0 or greater is required");
 #include <fmt/printf.h>
-#if FMT_VERSION >= 80000 || FMT_VERSION < 50300
-#include <utility> //std::forward
-#include "compiler_features.h"
-
-#define FMT_ADL_FUNCTION_NAME adapt_string_view
-#define OVERRIDE_FMT(func) template<typename... Args>\
-ForceInline decltype(auto) func(Args&&... args) {\
-	return fmt::func(FMT_ADL_FUNCTION_NAME(std::forward<Args>(args))...);\
-}
-template <typename T>
-constexpr ForceInline const T& FMT_ADL_FUNCTION_NAME(const T& obj) {
-    return obj;
-}
-#else
-#define FMT_ADL_FUNCTION_NAME to_string_view
-#define OVERRIDE_FMT(func) using fmt::func;
-#endif
 #if FMT_VERSION >= 80000
 #include <fmt/xchar.h>
 #endif
@@ -31,31 +14,29 @@ constexpr ForceInline const T& FMT_ADL_FUNCTION_NAME(const T& obj) {
 #else
 #include <fmt/time.h>
 #endif
-
-namespace nonstd { namespace sv_lite {
-template<class CharT, class Traits>
-class basic_string_view;
-template<typename T, typename Traits>
-inline fmt::basic_string_view<T> FMT_ADL_FUNCTION_NAME(const nonstd::sv_lite::basic_string_view<T, Traits>& s) {
-	return { s.data(), s.size() };
-}
-}}
+#ifdef FMT_UNICODE
+#undef FMT_UNICODE
+#endif
 
 namespace irr {namespace core {
-template<typename T, typename TAlloc>
+template<typename CharT, typename TAlloc>
 class string;
-template<typename T, typename TAlloc>
-inline fmt::basic_string_view<T> FMT_ADL_FUNCTION_NAME(const irr::core::string<T, TAlloc>& s) {
-	return { s.data(), s.size() };
-}
 }}
 
+template<typename CharT, typename TAlloc>
+struct fmt::formatter<irr::core::string<CharT, TAlloc>, CharT> {
+	template<typename ParseContext>
+	constexpr auto parse(ParseContext& ctx) { return ctx.begin(); }
+	template <typename ParseContext>
+	constexpr auto format(const irr::core::string<CharT, TAlloc>& s, ParseContext& ctx) const {
+		return format_to(ctx.out(), fmt::basic_string_view{s.data(), s.size()});
+	}
+};
+
 namespace epro {
-OVERRIDE_FMT(format)
-OVERRIDE_FMT(sprintf)
-OVERRIDE_FMT(print)
+using fmt::format;
+using fmt::sprintf;
+using fmt::print;
 }
-#undef OVERRIDE_FMT
-#undef FMT_ADL_FUNCTION_NAME
 
 #endif
