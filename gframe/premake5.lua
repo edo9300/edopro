753f74ddd56a17c609f0d150ed55cd45e3347365
Edoardo Lolletti <edoardo762@gmail.com>
2025-04-22 18:43:51 +0200

    Fix ios compilation

--- gframe/premake5.lua
+++ gframe/premake5.lua
@@ -46,7 +46,7 @@ local ygopro_config=function(static_core)
 	if _OPTIONS["covers"] then
 		defines { "DEFAULT_COVER_URL=" .. _OPTIONS["covers"] }
 	end
-	if _OPTIONS["discord"] then
+	if _OPTIONS["discord"] and not os.istarget("ios") then
 		defines { "DISCORD_APP_ID=" .. _OPTIONS["discord"] }
 	end
 	if _OPTIONS["update-url"] then
@@ -150,7 +150,7 @@ local ygopro_config=function(static_core)
 		defines "IRR_COMPILE_WITH_DX9_DEV_PACK"
 
 	filter "system:not windows"
-		if _OPTIONS["discord"] then
+		if _OPTIONS["discord"] and not os.istarget("ios") then
 			links "discord-rpc"
 		end
 		links { "sqlite3", "event", "event_pthreads", "dl", "git2", "ssh2" }
