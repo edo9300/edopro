f89929c2b29b5571dadd971692079d2c7828aa1a
Edoardo Lolletti <edoardo762@gmail.com>
2025-09-21 14:42:22 +0200

    Add FileStream and fileopen implementations to support content uris
    This change dwelves a bit into libc++'s internals, since its fstream implementation offers no way to generate a stream from a file descriptor (gcc), or from a FILE* handle (MSVC)

---
+++ gframe/Android/file_stream.cpp
@@ -0,0 +1,107 @@
+#include "../file_stream.h"
+#include "../log.h"
+#include "../fmt.h"
+#include <android_native_app_glue.h>
+#include <jni.h>
+#include <unistd.h>
+
+#if EDOPRO_ANDROID
+
+#include "access_private.hpp"
+#include "porting_android.h"
+
+ACCESS_PRIVATE_FIELD(std::filebuf, FILE*, __file_);
+ACCESS_PRIVATE_FIELD(std::filebuf, std::ios_base::openmode, __om_);
+
+static constexpr const char* make_mdstring(std::ios_base::openmode mode) {
+    using namespace std;
+    switch (mode) {
+    case ios_base::out:
+    case ios_base::out | ios_base::trunc:
+        return "w";
+    case ios_base::out | ios_base::app:
+    case ios_base::app:
+        return "a";
+    case ios_base::in:
+        return "r";
+    case ios_base::in | ios_base::out:
+        return "r+";
+    case ios_base::in | ios_base::out | ios_base::trunc:
+        return "w+";
+    case ios_base::in | ios_base::out | ios_base::app:
+    case ios_base::in | ios_base::app:
+        return "a+";
+    case ios_base::out | ios_base::binary:
+    case ios_base::out | ios_base::trunc | ios_base::binary:
+        return "wb";
+    case ios_base::out | ios_base::app | ios_base::binary:
+    case ios_base::app | ios_base::binary:
+        return "ab";
+    case ios_base::in | ios_base::binary:
+        return "rb";
+    case ios_base::in | ios_base::out | ios_base::binary:
+        return "r+b";
+    case ios_base::in | ios_base::out | ios_base::trunc | ios_base::binary:
+        return "w+b";
+    case ios_base::in | ios_base::out | ios_base::app | ios_base::binary:
+    case ios_base::in | ios_base::app | ios_base::binary:
+        return "a+b";
+    default:
+        return nullptr;
+    }
+}
+
+static constexpr const char* c_mode_to_java(epro::path_stringview mode) {
+	if(mode.back() == 'b')
+		mode.remove_suffix(1);
+	if(mode == "w") {
+		return "wt";
+	}
+	if(mode == "a") {
+		return "wa";
+	}
+	if(mode == "r") {
+		return "r";
+	}
+	if(mode == "r+") {
+		return "rw";
+	}
+	if(mode == "w+") {
+		return "rwt";
+	}
+	if(mode == "a+") {
+		return "rwa";
+	}
+	unreachable();
+}
+
+FileStream::FileStream(std::string filename, FileMode mode) : std::fstream() {
+	if(!porting::pathIsUri(filename)) {
+		open(filename, mode.streammode);
+		return;
+	}
+	FILE* file = fileopen(filename, make_mdstring(mode.streammode));
+	if(file == nullptr)
+		return;
+	// hacky things, manually set up the fstream base class members so that it's using
+	// our file handle under the hood
+    auto& rdbuf_ref = *rdbuf();
+    access_private::__file_(rdbuf_ref) = file;
+    access_private::__om_(rdbuf_ref) = mode.streammode;
+	clear();
+}
+
+FILE* fileopen(epro::path_stringview filename, epro::path_stringview mode) {
+	if(!porting::pathIsUri(filename)) {
+		return fopen(filename.data(), mode.data());
+	}
+	int fd = porting::openFdFromUri(filename, c_mode_to_java(mode));
+	if(fd == -1)
+		return nullptr;
+    auto* file = fdopen(fd, mode.data());
+	if(file)
+		return file;
+	close(fd);
+	return nullptr;
+}
+#endif
