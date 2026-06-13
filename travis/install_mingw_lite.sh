a7e46fa811013ae96b9ab7c44e947ed83dd24832
Edoardo Lolletti <edoardo762@gmail.com>
2026-06-13 10:29:32 +0200

    Update to mingw lite 16.1.0

--- travis/install_mingw_lite.sh
+++ travis/install_mingw_lite.sh
@@ -5,10 +5,10 @@ MINGW_LITE_VARIANT=${1:-$MINGW_LITE_VARIANT}
 
 cd /tmp
 
-curl -L https://github.com/redpanda-cpp/mingw-lite/releases/download/15.2.0-r7/x-mingw32$MINGW_LITE_VARIANT-15.2.0-r7.tar.zst -o mingw.tar.zst
+curl -L https://github.com/redpanda-cpp/mingw-lite/releases/download/16.1.0-r0/x-mingw32$MINGW_LITE_VARIANT-16.1.0-r0.tar.zst -o mingw.tar.zst
 
 tar -xvf mingw.tar.zst
 
-layers=(/tmp/mingw32$MINGW_LITE_VARIANT-15/AAB/{binutils,crt,gcc,headers}/usr/local)
+layers=(/tmp/mingw32$MINGW_LITE_VARIANT-16/AAB/{binutils,crt-target,gcc,gcc-lib,headers,mcfgthread,winpthreads}/usr/local)
 lowerdir=$(IFS=:; echo "${layers[*]}")
 sudo mount -t overlay none /usr/local -o lowerdir=$lowerdir
