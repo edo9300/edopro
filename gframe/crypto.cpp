58f6d1af4d782daeba86de58173fb448ee8c1ce3
Edoardo Lolletti <edoardo762@gmail.com>
2026-08-01 18:46:30 +0200

    add md5/sha implementation using openssl

--- gframe/crypto.cpp
+++ gframe/crypto.cpp
@@ -2,10 +2,23 @@
 
 #include <array>
 
+#ifdef EPRO_USE_OPENSSL_CRYPTO
+
+#include <openssl/evp.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+
+#else
+
 #include "MD5/md5.h"
+
+#endif
+
 #include "file_stream.h"
 
 namespace epro {
+
+#ifndef EPRO_USE_OPENSSL_CRYPTO
 
 MD5Context::MD5Context() {
 	static_assert(sizeof(digest) == MD5_DIGEST_LENGTH);
@@ -27,6 +40,53 @@ auto MD5Context::final() -> digest {
 	MD5_Final(result.data(), static_cast<MD5_CTX*>(ctx));
 	return result;
 }
+
+#else
+
+MD5Context::MD5Context() : CryptoContext(EVP_md5()) {
+	static_assert(sizeof(digest) == MD5_DIGEST_LENGTH);
+}
+
+SHA256Context::SHA256Context() : CryptoContext(EVP_sha256()) {
+	static_assert(sizeof(digest) == SHA256_DIGEST_LENGTH);
+}
+
+CryptoContext::CryptoContext(const void* evp) {
+	auto* evp_md_ctx = EVP_MD_CTX_new();
+	if(!EVP_DigestInit_ex(evp_md_ctx, static_cast<const EVP_MD*>(evp), nullptr)) {
+		EVP_MD_CTX_free(evp_md_ctx);
+		return;
+	}
+	ctx = evp_md_ctx;
+}
+
+CryptoContext::~CryptoContext() {
+	EVP_MD_CTX_free(static_cast<EVP_MD_CTX*>(ctx));
+}
+
+void CryptoContext::update(void* data, size_t len) {
+	EVP_DigestUpdate(static_cast<EVP_MD_CTX*>(ctx), data, len);
+}
+
+void CryptoContext::final(void* data) {
+	EVP_DigestFinal_ex(static_cast<EVP_MD_CTX*>(ctx), static_cast<unsigned char*>(data), nullptr);
+}
+
+SHA256Context::digest calculateSHA256(epro::path_stringview file) {
+	FileStream instream{ file.data(), FileStream::in | FileStream::binary };
+	if(instream.fail())
+		return {};
+
+	epro::SHA256Context context{};
+	std::array<char, 512> buff;
+	while(!instream.eof()) {
+		instream.read(buff.data(), buff.size());
+		context.update(buff.data(), static_cast<size_t>(instream.gcount()));
+	}
+	return context.final();
+}
+
+#endif
 
 MD5Context::digest calculateMD5(epro::path_stringview file) {
 	FileStream instream{ file.data(), FileStream::in | FileStream::binary };
--- gframe/crypto.h
+++ gframe/crypto.h
@@ -8,6 +8,8 @@
 
 namespace epro {
 
+#ifndef EPRO_USE_OPENSSL_CRYPTO
+
 class MD5Context {
 public:
 	using digest = std::array<uint8_t, 16>;
@@ -19,6 +21,50 @@ public:
 private:
 	void* ctx;
 };
+
+#else
+
+class CryptoContext {
+public:
+	~CryptoContext();
+
+	void update(void* data, size_t len);
+
+protected:
+	CryptoContext(const void* evp);
+	void final(void* buff);
+
+private:
+	void* ctx;
+
+};
+
+class MD5Context final : public CryptoContext {
+public:
+	using digest = std::array<uint8_t, 16>;
+	MD5Context();
+
+	digest final() {
+		digest ret;
+		CryptoContext::final(ret.data());
+		return ret;
+	}
+};
+
+class SHA256Context final : public CryptoContext {
+public:
+	using digest = std::array<uint8_t, 32>;
+	SHA256Context();
+
+	digest final() {
+		digest ret;
+		CryptoContext::final(ret.data());
+		return ret;
+	}
+};
+
+SHA256Context::digest calculateSHA256(epro::path_stringview file);
+#endif
 
 MD5Context::digest calculateMD5(epro::path_stringview file);
 
