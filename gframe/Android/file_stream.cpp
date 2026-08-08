f1e8b435598ebca9445bf82c77ab2d107be83f31
Edoardo Lolletti <edoardo762@gmail.com>
2026-08-08 17:59:13 +0200

    spaces->tab

--- gframe/Android/file_stream.cpp
+++ gframe/Android/file_stream.cpp
@@ -13,41 +13,41 @@ ACCESS_PRIVATE_FIELD(std::filebuf, FILE*, __file_);
 ACCESS_PRIVATE_FIELD(std::filebuf, std::ios_base::openmode, __om_);
 
 static constexpr const char* make_mdstring(std::ios_base::openmode mode) {
-    using namespace std;
-    switch (mode) {
-    case ios_base::out:
-    case ios_base::out | ios_base::trunc:
-        return "w";
-    case ios_base::out | ios_base::app:
-    case ios_base::app:
-        return "a";
-    case ios_base::in:
-        return "r";
-    case ios_base::in | ios_base::out:
-        return "r+";
-    case ios_base::in | ios_base::out | ios_base::trunc:
-        return "w+";
-    case ios_base::in | ios_base::out | ios_base::app:
-    case ios_base::in | ios_base::app:
-        return "a+";
-    case ios_base::out | ios_base::binary:
-    case ios_base::out | ios_base::trunc | ios_base::binary:
-        return "wb";
-    case ios_base::out | ios_base::app | ios_base::binary:
-    case ios_base::app | ios_base::binary:
-        return "ab";
-    case ios_base::in | ios_base::binary:
-        return "rb";
-    case ios_base::in | ios_base::out | ios_base::binary:
-        return "r+b";
-    case ios_base::in | ios_base::out | ios_base::trunc | ios_base::binary:
-        return "w+b";
-    case ios_base::in | ios_base::out | ios_base::app | ios_base::binary:
-    case ios_base::in | ios_base::app | ios_base::binary:
-        return "a+b";
-    default:
-        return nullptr;
-    }
+	using namespace std;
+	switch(mode) {
+		case ios_base::out:
+		case ios_base::out | ios_base::trunc:
+			return "w";
+		case ios_base::out | ios_base::app:
+		case ios_base::app:
+			return "a";
+		case ios_base::in:
+			return "r";
+		case ios_base::in | ios_base::out:
+			return "r+";
+		case ios_base::in | ios_base::out | ios_base::trunc:
+			return "w+";
+		case ios_base::in | ios_base::out | ios_base::app:
+		case ios_base::in | ios_base::app:
+			return "a+";
+		case ios_base::out | ios_base::binary:
+		case ios_base::out | ios_base::trunc | ios_base::binary:
+			return "wb";
+		case ios_base::out | ios_base::app | ios_base::binary:
+		case ios_base::app | ios_base::binary:
+			return "ab";
+		case ios_base::in | ios_base::binary:
+			return "rb";
+		case ios_base::in | ios_base::out | ios_base::binary:
+			return "r+b";
+		case ios_base::in | ios_base::out | ios_base::trunc | ios_base::binary:
+			return "w+b";
+		case ios_base::in | ios_base::out | ios_base::app | ios_base::binary:
+		case ios_base::in | ios_base::app | ios_base::binary:
+			return "a+b";
+		default:
+			return nullptr;
+	}
 }
 
 static constexpr const char* c_mode_to_java(epro::path_stringview mode) {
@@ -86,9 +86,9 @@ FileStream::FileStream(std::string filename, FileMode mode) : std::fstream() {
 	}
 	// hacky things, manually set up the fstream base class members so that it's using
 	// our file handle under the hood
-    auto& rdbuf_ref = *rdbuf();
-    access_private::__file_(rdbuf_ref) = file;
-    access_private::__om_(rdbuf_ref) = mode.streammode;
+	auto& rdbuf_ref = *rdbuf();
+	access_private::__file_(rdbuf_ref) = file;
+	access_private::__om_(rdbuf_ref) = mode.streammode;
 	clear();
 }
 
@@ -99,7 +99,7 @@ FILE* fileopen(epro::path_stringview filename, epro::path_stringview mode) {
 	int fd = porting::openFdFromUri(filename, c_mode_to_java(mode));
 	if(fd == -1)
 		return nullptr;
-    auto* file = fdopen(fd, mode.data());
+	auto* file = fdopen(fd, mode.data());
 	if(file)
 		return file;
 	close(fd);
