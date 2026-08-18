c1c66f6b50428cfde63cc24838b915e078f96b47
Edoardo Lolletti <edoardo762@gmail.com>
2026-08-18 16:20:46 +0200

    align deploy script with core

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,9 +4,20 @@ set -euo pipefail
 
 cd $DEPLOY_DIR
 git init
-git checkout --orphan $DEPLOY_BRANCH
+git remote add origin https://$DEPLOY_TOKEN@github.com/$DEPLOY_REPO.git
 git config user.email github-actions[bot]@users.noreply.github.com
 git config user.name "github-actions[bot]"
+echo $GITHUB_SHA > commit
+if ! git fetch -q origin $DEPLOY_BRANCH --depth 1; then
+	git checkout --orphan $DEPLOY_BRANCH
+else
+	mkdir ./.tmp
+	mv ./* ./.tmp
+	git checkout $DEPLOY_BRANCH
+	rm -r ./*
+	mv ./.tmp/* .
+	rm -r ./.tmp
+fi
 git add -A .
-git commit -qm "Deploy $DEPLOY_REPO to $DEPLOY_REPO:$DEPLOY_BRANCH"
-git push -qf https://$DEPLOY_TOKEN@github.com/$DEPLOY_REPO.git $DEPLOY_BRANCH:$DEPLOY_BRANCH
+git commit -qm "$GITHUB_SHA $DEPLOY_BRANCH"
+git push -qf origin $DEPLOY_BRANCH:$DEPLOY_BRANCH
