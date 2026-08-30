a57a008f8557885288599b1b57fb356c5c9a741c
Edoardo Lolletti <edoardo762@gmail.com>
2026-08-30 22:09:27 +0200

    fix compilation with openal framework

--- sfAudio/include/sfAudio/ALCheck.hpp
+++ sfAudio/include/sfAudio/ALCheck.hpp
@@ -29,8 +29,13 @@
 // Headers
 ////////////////////////////////////////////////////////////
 
+#if __has_include(<AL/al.h>)
 #include <AL/al.h>
 #include <AL/alc.h>
+#else
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#endif
 
 namespace sf
 {
